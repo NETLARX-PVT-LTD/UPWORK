ff6b7cdb3996e6ec053d4f10ceea9ae775f519f4
vikas <vikassijoriya2121@gmail.com>
2025-11-06 15:39:06 +0530

    API Config

---
+++ gobot/backend/src/Interface/Config/IConfigService.cs
@@ -0,0 +1,18 @@
+﻿// ---------------------------------------------------------------------
+// <copyright file="IBotService.cs" company="Netlarx">
+// Copyright (c) Netlarx softwares pvt ltd. All rights reserved.
+// </copyright>
+// ---------------------------------------------------------------------
+
+namespace Netlarx.Products.Gobot.Interface.Config
+{
+    using Netlarx.Products.Gobot.Errors;
+    using Netlarx.Products.Gobot.ModelDTO.Config;
+    using System;
+    using System.Threading.Tasks;
+
+    public interface IConfigService
+    {
+        Task<BotConfigResult> GetConfig(Guid botId, Errors errors);
+    }
+}
---
+++ gobot/backend/src/ModelDTO/Config/BotConfigResult.cs
@@ -0,0 +1,36 @@
+﻿// ---------------------------------------------------------------------
+// <copyright file="BotConfigResult.cs" company="Netlarx">
+// Copyright (c) Netlarx softwares pvt ltd. All rights reserved.
+// </copyright>
+// ------------------------------------------------------------------
+
+namespace Netlarx.Products.Gobot.ModelDTO.Config
+{
+    using Netlarx.Products.Gobot.Errors;
+    using Netlarx.Products.Gobot.Result;
+
+    public class BotConfigResult : Result
+    {
+        public BotConfigResult(bool success, string statusCode, BotConfigdto? botConfig, Errors? error = null)
+            : base(success, error, statusCode)
+        {
+            BotConfig = botConfig;
+        }
+
+        public BotConfigdto BotConfig { get; set; }
+    }
+    public class BotConfigdto
+    {
+        public BrandingResult Branding { get; set; }
+        public string WelcomeMessage { get; set; }
+        public string InputPlaceholder { get; set; }
+    }
+
+    public class BrandingResult
+    {
+        public string BotName { get; set; }
+        public string PrimaryColor { get; set; }
+        public string SecondaryColor { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
